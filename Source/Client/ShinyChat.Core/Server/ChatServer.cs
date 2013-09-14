c208420877b173658aa984abd51f34e023953e2a
jug-eared <cschmitz@360-websolutions.de>
2013-09-14 15:04:46 +0200

    Added Windsor support; Started Implementation of clientside retrieving of messages

--- Source/Client/ShinyChat.Core/Server/ChatServer.cs
+++ Source/Client/ShinyChat.Core/Server/ChatServer.cs
@@ -6,6 +6,10 @@ using ShinyChat.Common;
 using System.Net;
 using System.Net.Sockets;
 using ShinyChat.Common.Server;
+using System.Threading;
+using ShinyChat.Common.Entities;
+using System.IO;
+using System.Xml;
 
 namespace ShinyChat.Core.Server
 {
@@ -19,6 +23,13 @@ namespace ShinyChat.Core.Server
 
         private TcpClient _client;
         private List<IServerSubscriber> _subscribers;
+        private bool _connectionActive;
+
+        public IEnumerable<IServerMessage> IncomingMessages
+        {
+            get;
+            set;
+        }
 
         public int Port
         {
@@ -38,12 +49,6 @@ namespace ShinyChat.Core.Server
             set;
         }
 
-        public AsyncCallback MessageReceived
-        {
-            get;
-            set;
-        }
-
         public bool OpenConnection()
         {
             if (Endpoint != null && _client != null)
@@ -51,6 +56,9 @@ namespace ShinyChat.Core.Server
                 try
                 {
                     _client.Connect(Endpoint);
+                    _connectionActive = true;
+                    IncomingMessages = new List<IServerMessage>();
+                    _subscribers = new List<IServerSubscriber>();
                 }
                 catch (Exception ex)
                 {
@@ -69,6 +77,9 @@ namespace ShinyChat.Core.Server
                 try
                 {
                     _client.Close();
+                    _connectionActive = false;
+                    IncomingMessages = null;
+                    _subscribers = null;
                 }
                 catch (Exception ex)
                 {
@@ -82,7 +93,63 @@ namespace ShinyChat.Core.Server
 
         private void SocketListener()
         {
-            // TODO If message then notify subscribers
+            while (_connectionActive && _client.Connected)
+            {
+                Thread.Sleep(100);
+                if (_client.GetStream().DataAvailable)
+                {
+                    // Read Data from stream
+                    var optionsSizeBuffer = new byte[4];
+                    var contentSizeBuffer = new byte[4];
+
+                    _client.GetStream().Read(optionsSizeBuffer, 0, 4);
+                    _client.GetStream().Read(contentSizeBuffer, 0, 4);
+
+                    var optionsSize = Convert.ToUInt32(optionsSizeBuffer);
+                    var contentSize = Convert.ToUInt32(contentSizeBuffer);
+
+                    var optionsBuffer = new byte[optionsSize];
+                    var contentBuffer = new byte[contentSize];
+
+                    // Dirty typecast (OH NOEZ)
+                    _client.GetStream().Read(optionsBuffer, 0, (int)optionsSize);
+                    _client.GetStream().Read(contentBuffer, 0, (int)contentSize);
+
+                    // TODO Convert Data to IServerMessage
+                    // -- -- -- XML Reader
+                    var optionsText = System.Text.Encoding.UTF8.GetString(optionsBuffer);
+                    var contentText = System.Text.Encoding.UTF8.GetString(contentBuffer);
+
+                    var optionsDoc = new XmlDocument();
+                    optionsDoc.LoadXml(optionsText);
+                    var contentDoc = new XmlDocument();
+                    contentDoc.LoadXml(contentText);
+
+                    var resultMessage = new ServerMessage();
+                    try
+                    {
+                        resultMessage.Id = Convert.ToUInt32(optionsDoc.SelectSingleNode(@"\options\id").InnerText);
+                        resultMessage.MessageType = (Enums.MessageType)Convert.ToInt32(optionsDoc.SelectSingleNode(@"\options\messageType").InnerText);
+                        var commandInnerText = optionsDoc.SelectSingleNode(@"\options\command").InnerText;
+                        if (!string.IsNullOrEmpty(commandInnerText)) resultMessage.CommandType = (Enums.CommandType)Convert.ToUInt32(commandInnerText);
+                        
+                    }
+                    catch(Exception ex)
+                    {
+                        // Corrupt document -- ignore
+                        // TODO Log Error
+                    }
+
+
+                    // TODO Store IServerMessage in List
+
+                    // TODO If message then notify subscribers
+                    foreach (var subscriber in _subscribers)
+                    {
+                        subscriber.OnServerMessageReceived();
+                    }
+                }
+            }
         }
 
         public void Subscribe(IServerSubscriber subscriber)
@@ -94,6 +161,26 @@ namespace ShinyChat.Core.Server
         {
             if (_subscribers.Contains(subscriber))
                 _subscribers.Remove(subscriber);
+        }
+
+
+        public bool SendMessage(IServerMessage message)
+        {
+            if (_connectionActive && _client.Connected && message.SerializedMessage != null)
+            {
+                try
+                {
+                    // Dirty typecast (OH NOEZ AGAIN)
+                    _client.GetStream().Write(message.SerializedMessage, 0, 8 + (int)message.OptionsSize + (int)message.ContentSize);
+                }
+                catch (Exception ex)
+                {
+                    // TODO Log Error
+                    return false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
